05d90926e51f0cf8ee4ec55df1eea2183ee0eb09
ttt <994777249@qq.com>
2024-07-17 20:02:41 +0800

    动态展示完成

--- src/components/typeNav/index.vue
+++ src/components/typeNav/index.vue
@@ -47,18 +47,22 @@
                         <h3>
                             <a href="">{{ c1.categoryName }}</a>
                         </h3>
-                        <!-- <div class="item-list clearfix">
-                            <div class="subitem" v-for="(c2, index) in c1.categoryChild" :key="c2.categoryId"></div>
-                            <dl class="fore">
-                                <dt>
-                                    <a href="">{{ c2.categoryName }}</a>
-                                </dt>
-                                <dd>
-                                    <em v-for="(c3, index) in c2.categoryChild" :key="c3.categoryId"></em>
-                                    <a href="">{{ c3.categoryName }}</a>
-                                </dd>
-                            </dl>
-                        </div> -->
+                        <div class="item-list clearfix">
+                            <div class="subitem" v-for="(c2, index) in c1.categoryChild" :key="c2.categoryId">
+                                <dl class="fore">
+                                    <dt>
+                                        <a href="">{{ c2.categoryName }}</a>
+                                    </dt>
+                                    <dd>
+                                        <em v-for="(c3, index) in c2.categoryChild" :key="c3.categoryId">
+                                            <a href="">{{ c3.categoryName }}</a>
+                                        </em>
+
+                                    </dd>
+                                </dl>
+                            </div>
+
+                        </div>
                     </div>
 
                 </div>
@@ -76,11 +80,11 @@ export default {
 
     computed: {
         ...mapState({
-            // categoryList: state =>
-            //     state.home.categoryList.data
-            categoryList: (state) => {
-                console.log("jisuan", state);
-            }
+            categoryList: state =>
+                state.home.categoryList.data
+            // categoryList: (state) => {
+            //     console.log("jisuan", state);
+            // }
         })
     }
 
