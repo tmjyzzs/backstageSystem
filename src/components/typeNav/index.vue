34d1b71e3fc913383204ae6b6a954d9f2a293dcf
ttt <994777249@qq.com>
2024-07-18 10:17:59 +0800

    使用样式-鼠标悬停样式

--- src/components/typeNav/index.vue
+++ src/components/typeNav/index.vue
@@ -15,34 +15,6 @@
             </nav>
             <div class="sort">
                 <div class="all-sort-list2">
-                    <!-- <div class="item bo">
-                        <h3>
-                            <a href="">图书、音像、数字商品</a>
-                        </h3>
-                        <div class="item-list clearfix">
-                            <div class="subitem">
-                                <dl class="fore">
-                                    <dt>
-                                        <a href="">电子书</a>
-                                    </dt>
-                                    <dd>
-                                        <em>
-                                            <a href="">婚恋/两性</a>
-                                        </em>
-                                        <em>
-                                            <a href="">文学</a>
-                                        </em>
-                                        <em>
-                                            <a href="">经管</a>
-                                        </em>
-                                        <em>
-                                            <a href="">畅读VIP</a>
-                                        </em>
-                                    </dd>
-                                </dl>
-                            </div>
-                        </div>
-                    </div> -->
                     <div class="item" v-for="(c1, index) in categoryList" :key="c1.categoryId">
                         <h3>
                             <a href="">{{ c1.categoryName }}</a>
@@ -206,6 +178,9 @@ export default {
                             display: block;
                         }
                     }
+                }
+                .item:hover{
+                    background: red;
                 }
             }
         }
