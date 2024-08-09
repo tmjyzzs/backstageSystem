67c88c3a777846758609e32524c79db361526b12
ttt <994777249@qq.com>
2024-08-09 12:00:20 +0800

    讲路由抽离出来

--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -57,7 +57,9 @@
               <li class="yui3-u-1-5" v-for="(good, index) in goodsList" :key="good.id">
                 <div class="list-wrap">
                   <div class="p-img">
-                    <a href="item.html" target="_blank"><img :src="good.defaultImg" /></a>
+                    <!-- 商品详情页 -->
+                    <router-link :to="`/detail/${good.id}`"><img :src="good.defaultImg" />111</router-link>
+                    <!-- <a href="item.html" target="_blank"><img :src="good.defaultImg" /></a> -->
                   </div>
                   <div class="price">
                     <strong>
